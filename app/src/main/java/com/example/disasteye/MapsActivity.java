64fb56eedbb82396b34f4320aebebd95a6192b6c
EjobeTea <ejobe98@gmail.com>
2022-02-17 21:35:27 -0600

    Search Bar Implementation in Java. Still crashes after pressing enter.

--- app/src/main/java/com/example/disasteye/MapsActivity.java
+++ app/src/main/java/com/example/disasteye/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.example.disasteye;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -12,23 +15,69 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.disasteye.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        //R.id.idSearchView is found in the activity_maps.xml  -- Searches given location
+        //More on searchView doc: https://abhiandroid.com/ui/searchview#SearchView_Methods_In_Android
+        searchView = (SearchView) findViewById(R.id.idSearchView);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
+        //OnQueryTextListener() -- call backs to changed made in query text: https://developer.android.com/reference/android/widget/SearchView.OnQueryTextListener
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                String locationName = searchView.getQuery().toString();
+                ArrayList<Address> addressList = new ArrayList<>();
+
+                if (locationName.equals("") || locationName != null){
+                    // Create geocoder obj -- takes address and finds location: https://developer.android.com/reference/android/location/Geocoder
+                    Geocoder geocoder = new Geocoder(MapsActivity.this);
+
+                    //Given locationName, it gecodes the location on map, and adds to addressList
+                    try { addressList.add((Address) geocoder.getFromLocationName(locationName, 1)); }
+                    catch(Exception except){ except.printStackTrace(); }
+
+                    //Get location, from the first position listed in addressList:
+                    Address address = null;
+                    try{ address = (Address) addressList.get(0); }
+                    catch(IndexOutOfBoundsException exception){ exception.printStackTrace();}
+                    // Add location's cordinates:
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+                    // Add marker to pos.
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(locationName));
+
+                    // Animate camera to pos.
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                }
+                //Returns false to let search view perform default action:
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
         mapFragment.getMapAsync(this);
     }
+
 
     /**
      * Manipulates the map once available.
@@ -44,8 +93,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
