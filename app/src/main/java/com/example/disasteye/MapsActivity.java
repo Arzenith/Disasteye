ee0a3a6a6b6ba0d5facb32208eb15e839e5840f8
ahnafahmad <ahnafahmad0425@gmail.com>
2022-02-22 12:56:16 -0600

    test push

--- app/src/main/java/com/example/disasteye/MapsActivity.java
+++ app/src/main/java/com/example/disasteye/MapsActivity.java
@@ -94,7 +94,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
         //Trying to push stuff
-
+        // trying to push stuff 2
         // Add a marker in Sydney and move the camera
         //LatLng sydney = new LatLng(-34, 151);
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
