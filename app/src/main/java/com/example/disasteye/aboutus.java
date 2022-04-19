8e918821fc50c35e6c2391160463e63dad3580ee
EjobeTea <ejobe98@gmail.com>
2022-04-19 11:58:22 -0500

    updated back button.

--- app/src/main/java/com/example/disasteye/aboutus.java
+++ app/src/main/java/com/example/disasteye/aboutus.java
@@ -2,14 +2,9 @@ package com.example.disasteye;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.RelativeLayout;
-
-import com.google.android.material.appbar.MaterialToolbar;
 
 public class aboutus extends AppCompatActivity {
     @Override
